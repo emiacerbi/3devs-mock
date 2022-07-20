8e7472ff3ce64cd17508f6fd7672be26dc8eb293
Emiliano Acerbi <eacerbip@gmail.com>
2022-07-19 21:40:23 -0300

    format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useState } from "react"
 
-import { BsTwitter, BsFacebook, BsInstagram, BsWhatsapp } from 'react-icons/bs'
-import { IoMdSend } from 'react-icons/io'
+import { BsTwitter, BsFacebook, BsInstagram, BsWhatsapp } from "react-icons/bs"
+import { IoMdSend } from "react-icons/io"
 
 export const Footer = () => {
-  const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState("")
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value)
@@ -16,23 +16,37 @@ export const Footer = () => {
   }
 
   return (
-    <footer className='text-[#F8F6F1] bg-[#242325]'>
-      <div className='container flex flex-col gap-8 items-center py-10 mx-auto'>
-        <div className='flex flex-col gap-2 items-center'>
-          <h3 className='text-2xl text-[#D5A021]' >¿Qué quieres pedir?</h3>
+    <footer className="text-[#F8F6F1] bg-[#242325]">
+      <div className="container flex flex-col gap-8 items-center py-10 mx-auto">
+        <div className="flex flex-col gap-2 items-center">
+          <h3 className="text-2xl text-[#D5A021]">¿Qué quieres pedir?</h3>
           <p>Tu mensaje nos llega directamente a nuestro WhatsApp!</p>
         </div>
 
-        <form className='flex relative gap-5 items-center w-2/3' onSubmit={handleSubmit}>
-          <input className='p-5 w-[100%] text-xl text-black rounded-3xl outline-none' onChange={handleChange} />
-          <IoMdSend className='absolute right-[-10%] text-[4rem] text-blue-400 hover:text-blue-300 duration-200 cursor-pointer' />
+        <form
+          className="flex relative gap-5 items-center w-2/3"
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="p-5 w-[100%] text-xl text-black rounded-3xl outline-none"
+            onChange={handleChange}
+          />
+          <IoMdSend className="absolute right-[-10%] text-[4rem] text-blue-400 hover:text-blue-300 duration-200 cursor-pointer" />
         </form>
 
-        <ul className='flex gap-5'>
-          <li className='p-2 text-3xl hover:opacity-60 duration-200 cursor-pointer'><BsTwitter /></li>
-          <li className='p-2 text-3xl hover:opacity-60 duration-200 cursor-pointer'><BsFacebook /></li>
-          <li className='p-2 text-3xl hover:opacity-60 duration-200 cursor-pointer'><BsWhatsapp /></li>
-          <li className='p-2 text-3xl hover:opacity-60 duration-200 cursor-pointer'><BsInstagram /></li>
+        <ul className="flex gap-5">
+          <li className="p-2 text-3xl hover:opacity-60 duration-200 cursor-pointer">
+            <BsTwitter />
+          </li>
+          <li className="p-2 text-3xl hover:opacity-60 duration-200 cursor-pointer">
+            <BsFacebook />
+          </li>
+          <li className="p-2 text-3xl hover:opacity-60 duration-200 cursor-pointer">
+            <BsWhatsapp />
+          </li>
+          <li className="p-2 text-3xl hover:opacity-60 duration-200 cursor-pointer">
+            <BsInstagram />
+          </li>
         </ul>
       </div>
     </footer>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Button } from './utilities/Button'
+import React from "react"
+import { Button } from "./utilities/Button"
 
 export const Hero = () => {
   return (
-    <section className='flex min-h-screen'>
+    <section className="flex min-h-screen">
       {/* Bg Image */}
-      <div className='absolute -z-10 w-full h-full bg-[center_bottom] bg-no-repeat bg-cover bg-hero-pattern '></div>
+      <div className="absolute -z-10 w-full h-full bg-[center_bottom] bg-no-repeat bg-cover bg-hero-pattern "></div>
       {/* Shadow */}
-      <div className='absolute -z-10 w-full h-full bg-black opacity-30 '></div>
+      <div className="absolute -z-10 w-full h-full bg-black opacity-30 "></div>
 
-      <div className='container flex justify-start items-center px-10 mx-auto'>
-        <div className='flex flex-col gap-5 tracking-wide '>
-          <h1 className='max-w-[22ch] text-5xl text-[#F8F6F1] shadow-red-500 drop-shadow-md'>LA MEJOR CALIDAD ESTA A SU DISPOSICIÓN</h1>
+      <div className="container flex justify-start items-center px-10 mx-auto">
+        <div className="flex flex-col gap-5 tracking-wide ">
+          <h1 className="max-w-[22ch] text-5xl text-[#F8F6F1] shadow-red-500 drop-shadow-md">
+            LA MEJOR CALIDAD ESTA A SU DISPOSICIÓN
+          </h1>
           <Button>ELEGI Y PEDINOS</Button>
         </div>
       </div>
